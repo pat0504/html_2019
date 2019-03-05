9bc29e455d64d17e35bf036746d7c15268fe0e68
Patrick Deegan <pat667deegan@gmail.com>
2019-03-05 10:20:55 -0500

    My old index file with date and time

---
+++ timeDate.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Patrick PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello Patrick<br><a href=\'bitnami.html\'>see the Bitnami page</a></p>';
+ date_default_timezone_set('EST');
+echo date('H:i:s');
+echo '<br>';
+echo date('M, d, Y')?>
+ </body>
+</html>
